b9914196cda08fafac557d9665aaeda4308f958a
vishwajeet-8 <vishwajeetrout8@gmail.com>
2025-05-10 13:34:15 +0530

    fix cors policy

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello API is runing now with the help of pm2");
